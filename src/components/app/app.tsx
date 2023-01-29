fe7c9fb898f4ca47785ecb5a86c3f03ae86c28a2
Kemran Rustamov <rustamovkm@gmail.com>
2023-01-29 21:31:22 +0300

    step 2

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,14 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../../pages/main-screen/main-screen';
+
+type PlaceCradsCount = {
+  placeCradsCount: number;
 }
+
+function App({placeCradsCount}: PlaceCradsCount): JSX.Element {
+  return (
+    <MainScreen placeCradsCount={placeCradsCount} />
+  );
+}
+
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const Setting = {
+  PlaceCradsCount: 3,
+} as const;
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      placeCradsCount = {Setting.PlaceCradsCount}
+    />
   </React.StrictMode>,
 );
+
+
